8b2c82bb5ab0e176ca6b8fa4e0ae618c746bf6ab
David Lebee <david@poweredsoft.com>
2021-08-11 17:39:06 -0400

    in memory.

---
+++ PoweredSoft.Data.InMemory/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using PoweredSoft.Data.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PoweredSoft.Data.InMemory
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddPoweredSoftInMemoryDataServices(this IServiceCollection services)
+        {
+            services.AddPoweredSoftDataServices();
+            services.AddTransient<IAsyncQueryableHandlerService, InMemoryQueryableHandlerService>();
+            return services;
+        }
+    }
+}
