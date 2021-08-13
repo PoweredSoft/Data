7a7a6fa3f5089c30cc336f2a61641c905608777c
David Lebee <david@poweredsoft.com>
2021-08-13 02:58:40 -0400

    Upgrading to EF5.0

--- PoweredSoft.Data.EntityFrameworkCore/ASyncQueryableService.cs
+++ PoweredSoft.Data.EntityFrameworkCore/ASyncQueryableService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
 using Microsoft.EntityFrameworkCore.Query.Internal;
 using PoweredSoft.Data.Core;
 using System;
@@ -12,23 +13,23 @@ namespace PoweredSoft.Data.EntityFrameworkCore
 {
     public class AsyncQueryableHandlerService : IAsyncQueryableHandlerService
     {
-        public Task<T> FirstOrDefaultAsync<T>(IQueryable<T> queryable, CancellationToken cancellationToken = default(CancellationToken))
+        public Task<T> FirstOrDefaultAsync<T>(IQueryable<T> queryable, CancellationToken cancellationToken = default)
             => queryable.FirstOrDefaultAsync(cancellationToken);
-        public Task<T> FirstOrDefaultAsync<T>(IQueryable<T> queryable, Expression<Func<T, bool>> predicate, CancellationToken cancellationToken = default(CancellationToken)) 
+        public Task<T> FirstOrDefaultAsync<T>(IQueryable<T> queryable, Expression<Func<T, bool>> predicate, CancellationToken cancellationToken = default) 
             => queryable.FirstOrDefaultAsync(predicate, cancellationToken);
-        public Task<List<T>> ToListAsync<T>(IQueryable<T> queryable, CancellationToken cancellationToken = default(CancellationToken)) 
+        public Task<List<T>> ToListAsync<T>(IQueryable<T> queryable, CancellationToken cancellationToken = default) 
             => queryable.ToListAsync(cancellationToken);
-        public Task<int> CountAsync<T>(IQueryable<T> queryable, CancellationToken cancellationToken = default(CancellationToken))
+        public Task<int> CountAsync<T>(IQueryable<T> queryable, CancellationToken cancellationToken = default)
             => queryable.CountAsync();
-        public Task<long> LongCountAsync<T>(IQueryable<T> queryable, CancellationToken cancellationToken = default(CancellationToken))
+        public Task<long> LongCountAsync<T>(IQueryable<T> queryable, CancellationToken cancellationToken = default)
             => queryable.LongCountAsync();
 
         public bool CanHandle<T>(IQueryable<T> queryable) => queryable.Provider is IAsyncQueryProvider;
 
-        public Task<bool> AnyAsync<T>(IQueryable<T> queryable, Expression<Func<T, bool>> predicate, CancellationToken cancellationToken = default(CancellationToken))
+        public Task<bool> AnyAsync<T>(IQueryable<T> queryable, Expression<Func<T, bool>> predicate, CancellationToken cancellationToken = default)
             => queryable.AnyAsync(predicate, cancellationToken);
 
-        public Task<bool> AnyAsync<T>(IQueryable<T> queryable, CancellationToken cancellationToken = default(CancellationToken))
+        public Task<bool> AnyAsync<T>(IQueryable<T> queryable, CancellationToken cancellationToken = default)
             => queryable.AnyAsync(cancellationToken);
     }
 }
